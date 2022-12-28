90c9d488144c1c2b30ea2364899e43930e304601
Akash Yadav <aryan.yadav.boss999@gmail.com>
2022-12-28 09:23:59 +0530

    S

---
+++ SSTF.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<stdlib.h>
+#include<limits.h>
+int search(int req[],bool visited[],int head,int n)
+{
+    int ind =-1 , min_seek = INT_MAX , i ;
+    for(i=0;i<n;i++){
+        if(visited[i]==false && abs(head-req[i])<min_seek){
+            min_seek = abs(head-req[i]) ;
+            ind = i ;
+        }
+    }
+    return ind;
+}
+int main()
+{
+    int n , i , completed = 0 , head;
+    printf("Enter No. of Requests : ");
+    scanf("%d",&n);
+    int req[n] ; bool visited[n] ;
+    printf("Enter %d requests \n",n);
+    for(i=0;i<n;i++){
+        scanf("%d",&req[i]);
+        visited[i] = false ;
+    }
+    printf("Enter Head Position : ");
+    scanf("%d",&head);
+    int head_move = 0 ;
+    while(completed!=n)
+    {
+        int ind = search(req,visited,head,n) ;
+        if(ind!=-1){
+            visited[ind] = true ;
+            head_move += abs(head-req[ind]) ;
+            head = req[ind] ;
+            completed++ ;
+        }
+    }
+    printf("No. of Head Movements : %d \n",head_move);
+    printf("Average Head Movements : %.3f \n",(float)head_move/(float)n);
+    return 0 ;
+}
